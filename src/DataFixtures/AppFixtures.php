41206bae2163f90f9e5b9a9234a4334132d8c79c
marlene <marlene.mlg53@gmail.com>
2024-05-04 12:34:19 +0200

    authenticate (wip)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,26 +2,54 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Admin;
 use App\Entity\Customer;
 use App\Entity\Product;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // Création d'une vingtaine de User
-        for ($i = 0; $i < 20; $i++) {
-            $user = new User;
-            $user->setFirstname('Firstname : ' . $i);
-            $user->setLastname('Lastname : ' . $i);
-            $user->setEmail('Email : ' . $i);
-            $user->setPassword('Password : ' . $i);
-            $listUser[] = $user;
-            $manager->persist($user);
-        }
+
+        // // Création d'un user "normal"
+        $user = new User;
+        $user->setFirstname('henry');
+        $user->setLastname('dupont');
+        $user->setEmail("user@bookapi.com");
+        $user->setRoles(["ROLE_USER"]);
+        $listUser[] = $user;
+        $manager->persist($user);
+
+        // Création d'un user admin
+        $userAdmin = new Admin;
+        $userAdmin->setFirstname('Marlene');
+        $userAdmin->setLastname('Admin');
+        $userAdmin->setEmail("admin@bookapi.com");
+        $userAdmin->setRoles(["ROLE_ADMIN"]);
+        $userAdmin->setPassword($this->userPasswordHasher->hashPassword($userAdmin, "password"));
+        $manager->persist($userAdmin);
+
+        // // Création d'une vingtaine de User
+        // for ($i = 0; $i < 5; $i++) {
+        //     $user = new User;
+        //     $user->setFirstname('Firstname : ' . $i);
+        //     $user->setLastname('Lastname : ' . $i);
+        //     $user->setEmail('Email : ' . $i);
+        //     $user->setPassword('Password : ' . $i);
+        //     $listUser[] = $user;
+        //     $manager->persist($user);
+        // }
 
         // Création d'une vingtaine de Client
         for ($i = 0; $i < 20; $i++) {
--- src/Entity/Admin.php
+++ src/Entity/Admin.php
@@ -2,18 +2,17 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
+use App\Repository\AdminRepository;
 use DateTime;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 
-#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Entity(repositoryClass: AdminRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-class User implements UserInterface
+class Admin implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -32,6 +31,13 @@ class User implements UserInterface
     #[Groups(['customer:read'])]
     private array $roles = [];
 
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    #[Groups(['customer:read'])]
+    private ?string $password = null;
+
     #[ORM\Column(length: 255)]
     #[Groups(['customer:read'])]
     private ?string $firstname = null;
@@ -48,15 +54,8 @@ class User implements UserInterface
     #[Groups(['customer:read'])]
     private ?\DateTimeInterface $updated_at = null;
 
-    /**
-     * @var Collection<int, Customer>
-     */
-    #[ORM\ManyToMany(targetEntity: Customer::class, mappedBy: 'users')]
-    private Collection $customers;
-
     public function __construct()
     {
-        $this->customers = new ArrayCollection();
         $this->created_at = new DateTime();
         $this->updated_at = new DateTime();
     }
@@ -108,6 +107,21 @@ class User implements UserInterface
     public function setRoles(array $roles): static
     {
         $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
 
         return $this;
     }
@@ -170,29 +184,12 @@ class User implements UserInterface
     }
 
     /**
-     * @return Collection<int, Customer>
+     * Méthode getUsername qui permet de retourner le champ qui est utilisé pour l'authentification.
+     *
+     * @return string
      */
-    public function getCustomers(): Collection
+    public function getUsername(): string
     {
-        return $this->customers;
-    }
-
-    public function addCustomer(Customer $customer): static
-    {
-        if (!$this->customers->contains($customer)) {
-            $this->customers->add($customer);
-            $customer->addUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeCustomer(Customer $customer): static
-    {
-        if ($this->customers->removeElement($customer)) {
-            $customer->removeUser($this);
-        }
-
-        return $this;
+        return $this->getUserIdentifier();
     }
 }
