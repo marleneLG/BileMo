b8028122809e2312c728110ceeb5980d0a6b721e
marlene <marlene.mlg53@gmail.com>
2024-05-26 10:35:05 +0200

    Your project should use return types

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -21,7 +21,7 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
-    public function findAllWithPagination(int $page, int $limit)
+    public function findAllWithPagination(int $page, int $limit): int
     {
         $qb = $this->createQueryBuilder('u')
             ->setFirstResult(($page - 1) * $limit)
